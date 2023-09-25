ba0129971c4b87a7a1cdc426bc72cb42cf30b409
thanhphong3 <dcvhjb11223344@gamil.com>
2023-09-25 13:59:39 +0700

    Revert "Update LegoLogic.cs"
    This reverts commit 8a3af945ad12dfffcef7b6510df5468611051a19.

--- Assets/Scripts/LegoLogic.cs
+++ Assets/Scripts/LegoLogic.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class LegoLogic
 {
-    public static readonly Vector3 Grid = new Vector3(0.02f, 0.01f, 0.02f);
+    public static readonly Vector3 Grid = new Vector3(0.2f, 0.1f, 0.2f);
     public static int LayerMaskLego = LayerMask.GetMask("Lego");
 
     public static Vector3 SnapToGrid(Vector3 input)
